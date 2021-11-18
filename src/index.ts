2a92d71667fe009a3dafc4004ef1e5fa1c077621
Matheus Martins Sarmento <matheusms280100@gmail.com>
2021-11-17 23:09:56 -0300

    add swager for docs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerOptions from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 
 app.use("/users", usersRoutes);
 
