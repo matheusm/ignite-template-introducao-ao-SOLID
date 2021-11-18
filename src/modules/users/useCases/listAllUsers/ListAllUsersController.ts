aa5c5caf8f5111315945c0a86d126bab75b56162
Matheus Martins Sarmento <matheusms280100@gmail.com>
2021-11-17 21:13:36 -0300

    fix list all users

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,8 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const all = this.listAllUsersUseCase.execute();
+    const { user_id } = request.headers;
+    const all = this.listAllUsersUseCase.execute({ user_id: String(user_id) });
     return response.json(all);
   }
 }
