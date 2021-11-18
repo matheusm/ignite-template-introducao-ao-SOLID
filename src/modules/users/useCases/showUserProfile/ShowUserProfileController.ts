9f19777f33656760366d5454b210f553437362a9
Matheus Martins Sarmento <matheusms280100@gmail.com>
2021-11-17 21:25:20 -0300

    fix show user profile

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,11 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    const userProfile = this.showUserProfileUseCase.execute({
+      user_id: String(user_id),
+    });
+    return response.json(userProfile);
   }
 }
 
